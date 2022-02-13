e3c9966adef9d109e87b203e951d15e64ea61619
soc caroline <soc.caroline1991@gmail.com>
2022-02-13 17:09:25 +0100

    connexion base de donnée

--- back/db.js
+++ back/db.js
@@ -1,25 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Connexion BDD sequelize
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.database, process.env.user_sql, process.env.password_sql, {
+const sequelize = new Sequelize(
+  process.env.database,
+  process.env.user_sql,
+  process.env.password_sql,
+  {
     host: process.env.host_sql,
-    dialect: 'mysql' 
-  });
-
-  try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    dialect: "mysql",
   }
+);
 
-  module.exports = sequelize;
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
+module.exports = sequelize;
+ const Utilisateur = require('./models/User');
+ const Comment = require('./models/Comment');
+ const Article =require('./models/Article');
 
-  ///--------- TEST ---------///
+// relations
 
-
-// const chaton = sequelize.models.Utilisateur.build ({ mail: "rk@mail.fr", mot_psw: "milky", pseudonyme: "chaton"});
-// console.log(chaton); 
+//  Article.hasOne(Utilisateur,{
+//   foreignKey: 'id_user'
+// });
+// Utilisateur.belongsTo(Article);
+// //--- Lien commentaire avec Article
+// Comment.hasOne(Utilisateur,{
+//   foreignKey: 'id_user'
+// });
+// Utilisateur.belongsTo(Comment);
+// //--- Lien commentaire avec article
+// Comment.hasOne(Article,{
+//   foreignKey: 'id_article'
+// });
+// Article.belongsTo(Comment);
