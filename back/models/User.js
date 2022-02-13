e50cc5b47f036bc3a9437f1091ac13e73fbd37d4
soc caroline <soc.caroline1991@gmail.com>
2022-02-13 17:08:53 +0100

    test utilisateur

--- back/models/User.js
+++ back/models/User.js
@@ -1,88 +1,89 @@
-const sequelize = require("../db");
-const { Sequelize, Model, DataTypes } = require('sequelize');
-//const sequelize = new Sequelize('mysql');
-//const sequelize = new Sequelize("mysql::memory:");
-// const sequelize = new Sequelize('groupomania', 'root', 'root', {
-//     host: 'localhost',
-//     dialect: 'mysql'
-//   });
-const bcrypt = require('bcrypt');
+const sequelize = require("../db.js");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const bcrypt = require("bcrypt");
 
 class Utilisateur extends Model {
-    cryptPassword (req, res) {
-        bcrypt
-    // Cryptage du mot de passe 
-    .hash(this.password, 10)
-    // Creer nouvel utilisateur
-    .then((hash) => {
-       this.password = hash;
-    })
-    }
-}
-    
 
-Utilisateur.init({
+}
+
+Utilisateur.init(
+  {
     id_user: {
-    type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true,unique: true,
-    allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
     },
     mail: {
-    type: DataTypes.STRING(75),
-    allowNull: false,
+      type: DataTypes.STRING(75),
+      allowNull: false,
     },
     role: {
-    type: DataTypes.STRING(10),
-    defaultValue: "simpleuser",
-    allowNull: false,
+      type: DataTypes.STRING(10),
+      defaultValue: "simple_user",
+      allowNull: false,
     },
-    MOT_PSW: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
+    mot_psw: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      set(value) {
+        const hash = bcrypt.hashSync(value, 10);
+        this.setDataValue("mot_psw", hash);
+      },
     },
-    TEMP_PSW: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-    allowNull: false,
+    temp_psw: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
     },
-    DAT_CREA: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-    allowNull: false,
+    dat_crea: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
     },
-    DAT_MDP: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-    allowNull: false,
+    dat_mdp: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
     },
-    AVATAR: {
-    type: DataTypes.STRING(75),
-    defaultValue: "default.jpg",
-    allowNull: false,
+    avatar: {
+      type: DataTypes.STRING(75),
+      defaultValue: "default.jpg",
+      allowNull: false,
     },
-    PSEUDONYME: {
-    type: DataTypes.STRING(30),
-    allowNull: false,
+    pseudonyme: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
     },
-    POSTE: {
-    type: DataTypes.STRING(25),
-    allowNull: true,
+    poste: {
+      type: DataTypes.STRING(25),
     },
-    BUREAU: {
-    type: DataTypes.STRING(15),
-    allowNull: true,
+    bureau: {
+      type: DataTypes.STRING(15),
     },
-}, {
+  },
+  {
     // Other model options go here
     sequelize, // We need to pass the connection instance
-    modelName: 'Utilisateur', // We need to choose the model name    
-    tableName: 'utilisateur'
+    modelName: "Utilisateur", // We need to choose the model name
+    tableName: "utilisateur",
+    timestamps: false,
+  }
+);
+
+// (async () => {
+
+//   })();
+
+// console.log(Utilisateur === sequelize.models.Utilisateur);
+
+const chaton = Utilisateur.build({
+  mail: "m@mail.fr",
+  mot_psw: "milky",
+  pseudonyme: "chaton",
 });
-/*
-(async () => {
-    //await sequelize.sync({ force: true });
-    // Code here
-  })();*/
-//sequelize.models.utilisateur
-console.log(Utilisateur === sequelize.models.Utilisateur);
-const chaton = sequelize.models.Utilisateur.build ({ mail: "rk@mail.fr", mot_psw: "milky", pseudonyme: "chaton"});
-console.log(chaton); 
+console.log(chaton);
+
+// chaton.save();
