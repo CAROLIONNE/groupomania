145a1c8e8248f1e963e6500c22add4cad17c41c6
soc caroline <soc.caroline1991@gmail.com>
2022-02-15 15:05:41 +0100

    fonction article

--- back/controllers/article.js
+++ back/controllers/article.js
@@ -1,6 +1,5 @@
-
-// const Article = require('../models/Article')
-const Article = require('../models').Article
+const Article = require("../models/Article");
+// const Article = require('../models').Article
 // const { article } = require('../models');
 // const t = await sequelize.transaction();
 
@@ -8,6 +7,20 @@ const Article = require('../models').Article
 // console.log(article1);
 // article1.save()
 // .catch(err => console.log(err))
+
+exports.viewAllArticles = (req, res, next) => {
+  Article.findAll()
+    .then((articles) => {
+      if (!articles || articles.length === 0) {
+        res.status(404).json({ error: "Can't find any articles" });
+      }
+
+      res.status(200).json(articles);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "unable to access to the posts in DB" });
+    });
+};
 
 exports.ViewArticle = (req, res) => {
   const articleFound = Article.findOne({
@@ -23,18 +36,25 @@ exports.ViewArticle = (req, res) => {
   }
 };
 
-exports.viewAllArticles = (req, res, next) => {
-  Article.findAll()
-    .then((articles) => {
-      if (!articles || articles.length === 0) {
-        res.status(404).json({ error: "Can't find any articles" });
-      }
+module.exports.createArticle = (req, res) => {
+  const article = Article.create({
+    id_article: req.body.id_article,
+    id_user: req.body.id_user,
+    text: req.body.text,
+  }).catch((err) => console.log(err));
+};
 
-      res.status(200).json(articles);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "unable to access to the posts in DB" });
+module.exports.updateArticle = (req, res) => {
+  try {
+    const article = Article.update({
+      id_article: req.body.id_article,
+      id_user: req.body.id_user,
+      text: req.body.text,
     });
+    console.log("ID article:", comment.id_article);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.deleteArticle = async (req, res) => {
