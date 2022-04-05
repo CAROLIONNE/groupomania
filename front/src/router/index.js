251cffd76c430d2a0131476a91de1e03e062cb9b
soc caroline <soc.caroline1991@gmail.com>
2022-04-05 12:00:47 +0200

    route page 404

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,7 @@ import FilActu from '@/views/FilActu'
 import NewArticle from '@/views/NewArticle'
 import DisplayArticle from '@/views/DisplayArticle'
 import DisplayProfil from '@/views/DisplayProfil'
+import NotFound from '@/views/NotFound'
 
 Vue.use(VueRouter)
 
@@ -40,6 +41,15 @@ const routes = [
     name: 'DisplayProfil',
     component: DisplayProfil
   },    
+  {
+    path: '/*',
+    name: 'NotFound',
+    component: NotFound,
+    meta: {
+      title: '404 not Found'
+    }
+  },    
+
 ]
 
 const router = new VueRouter({
