5eacb8f99f223940c882beb9f04f61b2b8e76b30
soc caroline <soc.caroline1991@gmail.com>
2022-04-05 11:59:55 +0200

    fonction deconnexion automatique en cours

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,22 +9,26 @@
         />
       </router-link>
       <div id="navigation">
-        <div id="authentification">
+        <div id="authentification" v-if="!userConnect">
           <div id="inscription">
-            <router-link to="/inscription" id="ancre_inscription">Inscription</router-link>
+            <router-link to="/inscription" id="ancre_inscription"
+              >Inscription</router-link
+            >
           </div>
           <div id="connexion">
             <router-link to="/" id="ancre_connexion">Connexion</router-link>
           </div>
         </div>
-        <div id="fil_actu">
+        <div id="fil_actu" v-if="userConnect">
           <!-- <router-link to="/articles" id="ancre_fil_actu">Fil d'actualité</router-link> -->
-          <router-link :to="{ name: 'FilActu' }" id="ancre_fil_actu" >Fil d'actualité</router-link>
+          <router-link :to="{ name: 'FilActu' }" id="ancre_fil_actu">Fil d'actualité</router-link>
         </div>
-        <!-- <div id="ancre_profil" @click="Profil()"> -->
-        <div id="profil">
-          <router-link id="ancre_profil" :to="{ name: 'DisplayProfil', params: { id } }">Profil</router-link>
-        </div>
+        <a id="ancre_profil" @click="Profil()" v-if="userConnect">
+          Profil
+          <!-- <div id="profil"> -->
+          <!-- <router-link id="ancre_profil" :to="{ name: 'DisplayProfil', params: { id } }">Profil</router-link> -->
+        </a>
+        <a id="ancre_logout" @click="logOut()" v-if="userConnect">Deconnexion</a>
       </div>
     </nav>
     <router-view />
@@ -32,15 +36,33 @@
 </template>
 
 <script>
-let userJson = localStorage.getItem("user");
-let user = JSON.parse(userJson);
-// let token = user.token;
-let id = user.userID;
+// deconnexion auto au bout de 20 minutes
+setTimeout(function () {
+      localStorage.clear();
+      window.location = "/"
+      // this.$router.push({ name: "Connect" });
+  }, 1200000); 
+
+// setTimeout(logOut(), 1200000); 
+
 export default {
   name: "App",
+   data() {
+    return {
+      userConnect: localStorage.getItem("user")
+    };
+  },
   methods: {
     Profil() {
-      this.$router.push({ name: "DisplayProfil", params: { id } });
+      let user = JSON.parse(localStorage.getItem("user"));
+      let id = user.userID;
+      if (user) {
+        this.$router.push({ name: "DisplayProfil", params: { id } });
+      }
+    },
+    logOut() {
+      localStorage.clear();
+      this.$router.push({ name: "Connect" });
     },
   },
 };
