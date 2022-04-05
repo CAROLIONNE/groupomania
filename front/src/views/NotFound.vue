7d884a47b1186d446105979d0f84e43e711413fa
soc caroline <soc.caroline1991@gmail.com>
2022-04-05 11:53:10 +0200

    vue page 404

---
+++ front/src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1> Cette page est introuvable </h1>
+    <p>
+    Redirection vers la page de connexion
+    <router-link to="/" >
+     ici
+    </router-link>
+    </p>
+    <!-- si utilisateur connecté afficher-->
+    <p>
+    Redirection vers le fil d'actualité 
+    <router-link :to="{ name: 'FilActu' }">
+     ici
+    </router-link>
+    </p>
+    <router-view />
+</div>
+</template>
