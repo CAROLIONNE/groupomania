a73fa61ff4a511e70492553d056f8055e8f68fd1
soc caroline <soc.caroline1991@gmail.com>
2022-04-15 17:38:14 +0200

    dump BDD

---
+++ Dump20220415.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_groupomania
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+  `id_article` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL,
+  `date_crea` datetime NOT NULL,
+  `date_mod` datetime DEFAULT NULL,
+  `titre` varchar(150) NOT NULL DEFAULT 'title',
+  `media` varchar(150) DEFAULT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id_article`),
+  UNIQUE KEY `id_article` (`id_article`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `utilisateur` (`ID_USER`)
+) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commentaire` (
+  `id_commentaire` int NOT NULL AUTO_INCREMENT,
+  `id_article` int NOT NULL,
+  `id_user` int NOT NULL,
+  `date_crea` datetime NOT NULL,
+  `date_mod` datetime DEFAULT NULL,
+  `titre` varchar(150) NOT NULL DEFAULT 'title',
+  `text` text NOT NULL,
+  PRIMARY KEY (`id_commentaire`),
+  UNIQUE KEY `id_commentaire` (`id_commentaire`),
+  KEY `id_article` (`id_article`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `commentaire_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`),
+  CONSTRAINT `commentaire_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `utilisateur` (`ID_USER`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+DROP TABLE IF EXISTS `sequelizemeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `ID_USER` int NOT NULL AUTO_INCREMENT,
+  `MAIL` varchar(75) NOT NULL,
+  `ROLE` varchar(10) NOT NULL DEFAULT '0',
+  `MOT_PSW` varchar(70) NOT NULL,
+  `TEMP_PSW` tinyint(1) NOT NULL DEFAULT '1',
+  `DATE_CREA` datetime NOT NULL,
+  `DATE_MDP` datetime NOT NULL,
+  `AVATAR` varchar(75) DEFAULT 'default.jpg',
+  `PSEUDONYME` varchar(75) NOT NULL,
+  `POSTE` varchar(30) DEFAULT NULL,
+  `BUREAU` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`ID_USER`),
+  UNIQUE KEY `MAIL` (`MAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-15 17:37:00
