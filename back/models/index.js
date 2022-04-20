69982b2678609a9916afd0cc7a8948e78ac580dd
soc caroline <soc.caroline1991@gmail.com>
2022-04-20 16:03:43 +0200

    test relation regroupée

--- back/models/index.js
+++ back/models/index.js
@@ -8,6 +8,31 @@ Article.hasMany(Comment);
 Article.belongsTo(User);
 Comment.belongsTo(User);
 Comment.belongsTo(Article);
+// User.belongsTo(Article, {
+//     through: Article,
+//     foreignKey: 'id_user'
+// });
+// User.hasMany(Comment, {
+//     through: Comment,
+//     foreignKey: 'id_user'
+// });
+// Article.hasMany(Comment, {
+//     through: Comment,
+//     foreignKey: 'id_article'
+// });
+// Article.belongsTo(User, {
+//     through: User,
+//     foreignKey: 'id_user'
+// });
+// Comment.belongsTo(User, {
+//     through: User,
+//     foreignKey: 'id_user'
+// });
+// Comment.belongsTo(Article, {
+//     through: Article,
+//     foreignKey: 'id_article'
+// });
+
 
 
 module.exports = {
