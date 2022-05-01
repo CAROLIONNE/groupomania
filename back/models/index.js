426dfd6897545bfccbb0e56a899ce70c8ac26c4f
soc caroline <soc.caroline1991@gmail.com>
2022-05-01 17:49:24 +0200

    delete on cascade

--- back/models/index.js
+++ back/models/index.js
@@ -2,12 +2,12 @@ const Utilisateur = require('../models/User');
 const Article = require('../models/Article');
 const Comment = require('../models/Comment');
 
-Utilisateur.hasMany(Article);
-Utilisateur.hasMany(Comment);
-Article.hasMany(Comment);
-Article.belongsTo(Utilisateur);
-Comment.belongsTo(Utilisateur);
-Comment.belongsTo(Article);
+Utilisateur.hasMany(Article, { onDelete: "cascade"});
+Utilisateur.hasMany(Comment, { onDelete: "cascade"});
+Article.hasMany(Comment, { onDelete: "cascade"});
+Article.belongsTo(Utilisateur, { onDelete: "cascade"});
+Comment.belongsTo(Utilisateur, { onDelete: "cascade"});
+Comment.belongsTo(Article, { onDelete: "cascade"});
 
 module.exports = {
     Utilisateur,
