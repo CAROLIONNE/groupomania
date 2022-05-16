1ef783c24c3069540142006abdea833f8997d114
soc caroline <soc.caroline1991@gmail.com>
2022-05-16 09:03:22 +0200

    dump sans data

--- Dump20220513.sql
+++ Dump20220513.sql
@@ -32,19 +32,9 @@ CREATE TABLE `articles` (
   `text` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `utilisateurId` (`utilisateurId`),
-  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`utilisateurId`) REFERENCES `utilisateurs` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`utilisateurId`) REFERENCES `utilisateurs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `articles`
---
-
-LOCK TABLES `articles` WRITE;
-/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` VALUES (2,1,'2022-04-28 15:31:39','2022-04-30 19:50:07','hey','http://localhost:3000/images/goeland1651348207590.jpg','hoooo');
-/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `commentaires`
@@ -63,20 +53,10 @@ CREATE TABLE `commentaires` (
   PRIMARY KEY (`id`),
   KEY `utilisateurId` (`utilisateurId`),
   KEY `articleId` (`articleId`),
-  CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`utilisateurId`) REFERENCES `utilisateurs` (`id`),
-  CONSTRAINT `commentaires_ibfk_2` FOREIGN KEY (`articleId`) REFERENCES `articles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`utilisateurId`) REFERENCES `utilisateurs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `commentaires_ibfk_2` FOREIGN KEY (`articleId`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `commentaires`
---
-
-LOCK TABLES `commentaires` WRITE;
-/*!40000 ALTER TABLE `commentaires` DISABLE KEYS */;
-INSERT INTO `commentaires` VALUES (8,2,1,'2022-04-29 11:35:41','2022-04-30 19:44:02','ljl');
-/*!40000 ALTER TABLE `commentaires` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `utilisateurs`
@@ -99,18 +79,8 @@ CREATE TABLE `utilisateurs` (
   `bureau` char(15) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mail` (`mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `utilisateurs`
---
-
-LOCK TABLES `utilisateurs` WRITE;
-/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-INSERT INTO `utilisateurs` VALUES (1,'belmont@romain.fr',0,'$2b$10$3ISU572K63liIfa3IFAl2O1jWH4D0QVdN84MvMeFgJbRioEvAlBde',0,'2022-04-28 13:17:05','2022-04-30 20:47:34','img_profil1651351654242.jpg','belmont','developpeur','100'),(2,'caro@free.Fr',0,'$2b$10$3xt.heekZcswbs0OmtYjjegTh9KrWxikDMfGXn61Ik/R0sOj7gE6O',0,'2022-04-30 20:48:57','2022-04-30 20:48:57','default.png','CARO',NULL,NULL);
-/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -121,4 +91,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-01 14:20:47
+-- Dump completed on 2022-05-13 13:54:08
