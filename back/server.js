b9bbcdff59e9340a8e67711ee3d3c0ddc3650ba0
soc caroline <soc.caroline1991@gmail.com>
2022-05-28 14:33:07 +0200

    express rate limit

--- back/server.js
+++ back/server.js
@@ -21,9 +21,21 @@ app.use("/api/user", userRoutes);
 app.use('/api/article', articleRoutes);
 app.use('/api/comment', commentRoutes);
 
-// import dotenv
+// Import dotenv
 require("dotenv").config();
 
+// Limitation du nombre de requêtes
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
+app.use(limiter)
 
 
 // Ecoute du serveur
